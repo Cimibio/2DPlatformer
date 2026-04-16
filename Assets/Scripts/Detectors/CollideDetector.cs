2ed4957d8cee8964bf6311942658b901ce670c5e
Cimibio <shingaryovan@gmail.com>
2026-04-16 22:54:16 +1200

    commit 19.3
    added LayerMask to collideDetector

--- Assets/Scripts/Detectors/CollideDetector.cs
+++ Assets/Scripts/Detectors/CollideDetector.cs
@@ -3,25 +3,38 @@ using UnityEngine;
 
 public class CollideDetector : MonoBehaviour
 {
+    [SerializeField] private LayerMask _targetLayer;
+    [SerializeField] private bool _debugMode = true;
+
     private Collider2D _collider;
+    private int _targetLayerValue;
 
     public event Action Collided;
 
-    private void Start()
+    private void Awake()
     {
+        _targetLayerValue = _targetLayer.value;
+
         if (TryGetComponent(out Collider2D collider))
         {
             _collider = collider;
             _collider.isTrigger = true;
         }
+        else
+        {
+            Debug.LogError($"[{gameObject.name}] CollideDetector requires a Collider2D component!");
+            enabled = false;
+        }
     }
 
-    private void OnTriggerEnter2D(Collider2D collider)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (collider.TryGetComponent<Player>(out _))
+        if ((_targetLayerValue & (1 << other.gameObject.layer)) != 0)
         {
             Collided?.Invoke();
-            Debug.Log($"Detector detect collision with {collider.gameObject.name}");
+
+            if (_debugMode)
+                Debug.Log($"[{gameObject.name}] Detected collision with {other.gameObject.name}");
         }
     }
 }
